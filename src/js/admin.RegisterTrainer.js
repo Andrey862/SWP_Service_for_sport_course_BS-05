20c3d386bec5825687389545374f799b28d6f358
Ivan Abramov <I.abramov@innopolis.university>
2020-05-04 12:57:25 +0300

    add trainer registration

---
+++ src/js/admin.RegisterTrainer.js
@@ -0,0 +1,138 @@
+function Course(props) {
+
+    return (
+        <div className="card">
+            <div className="container1">
+                <h3 className="name">{props.course.name}</h3>
+                <p>{props.course.trainer}</p>
+                <p className="schedule">{props.course.schedule}</p>
+            </div>
+
+
+            <div className="container2">
+                <button className="btn" id={props.course.id}><a>Change</a></button>
+            </div>
+
+
+            <div className="container3">
+                <div>
+                    <h3>New Schedule</h3>
+                    <input className="input" type="text" id={"schedule" + props.course.id}/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+class App extends React.Component {
+
+    // state = getAllCourses()
+
+    render() {
+        return (
+
+
+            <div className="app">
+
+                {/*<div className="container5">*/}
+
+                {/*	<div className="title">*/}
+                {/*		<h2> Change schedule </h2>*/}
+                {/*	</div>*/}
+
+                {/*	<div className="list">*/}
+                {/*		{this.state.courses.map(course => {*/}
+                {/*			return (*/}
+                {/*				<Course course={course} key={course.name + Math.random()}/>*/}
+                {/*			)*/}
+                {/*		})}*/}
+                {/*	</div>*/}
+                {/*</div>*/}
+
+                <div className="container6">
+
+                    <div className="title2">
+                        <h2> Register trainer </h2>
+                    </div>
+
+                    <div className="card2">
+
+
+                        <div className="container7">
+
+                            <h3>First name</h3>
+                            <input className="input" type="text" id="firstName"/>
+
+                            <h3>Last name</h3>
+                            <input className="input" type="text" id="lastName"/>
+
+                            <h3>Email</h3>
+                            <input className="input" type="text" id="email"/>
+
+                            <h3>Password</h3>
+                            <input className="input" type="text" id="password"/>
+                        </div>
+
+                        <div className="container8">
+                            <button className="btn" id="RegisterTrainer"><a>Register</a></button>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+        )
+    }
+}
+
+
+//function add event handler on buttons clicks
+function addEventHandler() {
+    document.getElementById("RegisterTrainer").onclick = registerTrainer
+}
+
+
+async function registerTrainer() {
+    const url = `http://194.87.102.88/api/register/`
+    //
+    const data = {
+        first_name: document.getElementById("firstName").value,
+        last_name: document.getElementById("lastName").value,
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value
+    }
+
+    if (data.first_name && data.last_name && data.email && data.password) {
+        const json = JSON.stringify(data);
+
+        let response = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers:
+                {
+                    "Content-type": "application/json; charset=UTF-8"
+                }
+        });
+
+        let text = await response.text()
+
+
+        alert(
+            `${text}`
+        )
+
+    } else {
+        alert(`Please, fill all fields`)
+        return
+    }
+}
+
+
+ReactDOM.render(<App/>, document.getElementById('root'))
+
+addEventHandler()
+
+
+
--- src/js/main.js
+++ src/js/main.js
@@ -1,46 +1,119 @@
 const inputs = document.querySelectorAll('.input');
 
-function focusFunc(){
-	let parent = this.parentNode.parentNode;
-	parent.classList.add('focus');
+function focusFunc() {
+    let parent = this.parentNode.parentNode;
+    parent.classList.add('focus');
 }
 
-function blurFunc(){
-	let parent = this.parentNode.parentNode;
-	if(this.value == ""){
-		parent.classList.remove('focus');
-	}
+function blurFunc() {
+    let parent = this.parentNode.parentNode;
+    if (this.value == "") {
+        parent.classList.remove('focus');
+    }
 }
 
-inputs.forEach(input =>{
-	input.addEventListener('focus', focusFunc);
-	input.addEventListener('blur', blurFunc);
+inputs.forEach(input => {
+    input.addEventListener('focus', focusFunc);
+    input.addEventListener('blur', blurFunc);
 });
 
 
-document.getElementById('login_btn').onclick = function validation() {
-	var username = document.getElementById('username').value;
-	var password = document.getElementById('password').value;
+document.getElementById('login_btn').onclick = async function validation() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
 
-	var admin_u = "admin";
-	var admin_p = "admin";
+    var admin_u = "admin";
+    var admin_p = "admin";
 
-	var trainer_u = "trainer";
-	var trainer_p = "trainer";
+    var trainer_u = "trainer";
+    var trainer_p = "trainer";
 
-	var student_u = "student";
-	var student_p = "student";
+    var student_u = "student";
+    var student_p = "student";
 
-	if (username == admin_u && password == admin_p) {
-		window.location.href = 'admin.html';
-	} else if (username == trainer_u && password == trainer_p) {
-		window.location.href = 'trainer.html';
-	} else if (username == student_u && password == student_p) {
-		window.location.href = 'student.html';
-	} else {
-		document.getElementById('notice').innerHTML = "Wrong username or/and password";
-	}
+    let data = await authentification(username, password)
+    let id = data['id']
+    let token = data['token']
+
+    // alert(`Get: ${id} ${token}`)
+
+    let typeOfUser = await getTypeOfUser(id, token)
+
+    alert(`Type: ${typeOfUser}`)
+
+    switch (typeOfUser) {
+        case "student": {
+            window.location.href = 'student.html';
+        }
+        case "admin": {
+            window.location.href = "admin.html"
+        }
+        case "trainer": {
+            window.location.href = "trainer.html"
+        }
+    }
+
+    // if (username == admin_u && password == admin_p) {
+    //     window.location.href = 'admin.html';
+    // } else if (username == trainer_u && password == trainer_p) {
+    //     window.location.href = 'trainer.html';
+    // } else if (username == student_u && password == student_p) {
+    //     window.location.href = 'student.html';
+    // } else {
+    //     document.getElementById('notice').innerHTML = "Wrong username or/and password";
+    // }
 }
+
+async function authentification(email, password) {
+    let url = "http://194.87.102.88/api/auth/"
+
+
+    const data = {
+        email: email,
+        password: password
+    }
+
+    const json = JSON.stringify(data);
+
+    let response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers:
+            {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+    });
+
+    let text = await response.json()
+
+    return text
+}
+
+async function getTypeOfUser(id, token) {
+    let url = `http://194.87.102.88/api/users/${id}`;
+
+
+    alert(`Get type: ${id} ${token}`);
+
+    // const data = {
+    //     id: id
+    // }
+
+    const json = JSON.stringify(data);
+
+    let response = await fetch(url, {
+        method: 'GET',
+        headers:
+            {
+                'Authorization': `Token ${token}`
+            }
+    });
+
+    let text = await response.json();
+
+    return text
+}
+
 
 document.getElementById('my_statistics').onclick = function validation() {
     window.location.href = 'student.statistics.html';
